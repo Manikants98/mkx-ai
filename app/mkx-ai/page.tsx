424bb0f23b54ea7e85559d9c086c67a80d788619
Mani Kant Sharma <manikants157@gmail.com>
2025-05-13 08:10:15 +0530

    mkx

--- app/mkx-ai/page.tsx
+++ app/mkx-ai/page.tsx
@@ -174,7 +174,7 @@ export default function MkxAI() {
       <CssBaseline />
       {conversation.length === 0 ? (
         <div className="h-screen w-full flex flex-col gap-7 justify-center items-center transition-colors duration-300">
-          <div className="flex flex-col dark:bg-[#121212] bg-white rounded-lg p-4 items-center">
+          <div className="flex flex-col rounded-lg p-4 items-center">
             <p className="text-[1.875rem] font-bold bg-gradient-to-r from-[#9333ea] via-[#ec4899] to-[#3b82f6] bg-clip-text text-transparent animate-pulse">
               MKx AI
             </p>
@@ -257,7 +257,7 @@ export default function MkxAI() {
         </div>
       ) : (
         <div className="h-screen w-full flex flex-col gap-7 py-2 justify-between items-center transition-colors duration-300">
-          <p className="text-[1.875rem] sticky top-0 font-bold bg-gradient-to-r from-[#9333ea] via-[#ec4899] to-[#3b82f6] bg-clip-text text-transparent animate-pulse">
+          <p className="text-[1.875rem] dark:bg-[#121212] bg-white w-full sticky top-0 font-bold bg-gradient-to-r from-[#9333ea] via-[#ec4899] to-[#3b82f6] bg-clip-text text-transparent animate-pulse">
             MKx AI
           </p>
           <div
